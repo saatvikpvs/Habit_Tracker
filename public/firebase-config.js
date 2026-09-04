ab60940147bb79b4a343755e049657031b614e9d
saatvikpvs <pvssaatvik@gmail.com>
2026-09-04 17:26:55 +0530

    added fbconfig.js

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmquf-sAWb0NhKWdBH3DU2m6LIdl6If7U",
+  authDomain: "habit-tracker-62109.firebaseapp.com",
+  projectId: "habit-tracker-62109",
+  storageBucket: "habit-tracker-62109.firebasestorage.app",
+  messagingSenderId: "55707611483",
+  appId: "1:55707611483:web:b2e03fd2e88a604cb66a7e"
+};
